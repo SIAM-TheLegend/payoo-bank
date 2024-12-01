5be329fbe271a155d8c8f4492adeacdd087e5aeb
Shahriar Hasan (Siam) <siamshahriarhasan@gmail.com>
2024-12-02 04:09:31 +0600

    practice project initiated

---
+++ scripts/login.js
@@ -0,0 +1,11 @@
+document.getElementById("btn-login").addEventListener("click", function (event) {
+  event.preventDefault();
+  const number = document.getElementById("number").value;
+  const pin = document.getElementById("pin").value;
+
+  const myNum = "01972005986";
+  const myPin = "1234";
+  // if (myNum === number && myPin === pin) {
+  //   console.log(He)
+  // }
+});
