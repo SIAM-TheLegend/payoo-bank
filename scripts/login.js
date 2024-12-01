dff5b4a81e8025241b6d735e73f0dfc52e9414c5
Shahriar Hasan (Siam) <siamshahriarhasan@gmail.com>
2024-12-02 04:35:06 +0600

    fixed - authentication successful

--- scripts/login.js
+++ scripts/login.js
@@ -1,11 +1,14 @@
 document.getElementById("btn-login").addEventListener("click", function (event) {
   event.preventDefault();
+
   const number = document.getElementById("number").value;
   const pin = document.getElementById("pin").value;
 
   const myNum = "01972005986";
   const myPin = "1234";
-  // if (myNum === number && myPin === pin) {
-  //   console.log(He)
-  // }
+
+  console.log(myNum, number);
+  if (myNum === number && myPin === pin) {
+    console.log(myNum, number);
+  }
 });
