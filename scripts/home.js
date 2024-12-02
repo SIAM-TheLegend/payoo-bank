33fcfc29f34b884bed66eedd6b1c5f3d2c06f129
Shahriar Hasan (Siam) <siamshahriarhasan@gmail.com>
2024-12-02 21:20:10 +0600

    added - switching between tabs working properly

--- scripts/home.js
+++ scripts/home.js
@@ -1,14 +1,25 @@
 const myPin = "1234";
 const btnAddMoney = document.getElementById("btn-addMoney");
 const btnCashOut = document.getElementById("btn-cashOut");
+const addMoneyTab = document.getElementById("btn-add-money");
+const cashOutTab = document.getElementById("btn-cash-out");
+const addMoneySection = document.getElementById("section-add-money");
+const cashOutSection = document.getElementById("section-cash-out");
+
+addMoneyTab.addEventListener("click", function () {
+  addMoneySection.classList.remove("hidden");
+  cashOutSection.classList.add("hidden");
+});
+cashOutTab.addEventListener("click", function () {
+  cashOutSection.classList.remove("hidden");
+  addMoneySection.classList.add("hidden");
+});
 
 btnAddMoney.addEventListener("click", function (event) {
   event.preventDefault();
-
   let balance = document.getElementById("balance");
   const amount = document.getElementById("amount").value;
   const pin = document.getElementById("pin").value;
-
   if (myPin === pin) {
     balance.innerText = parseFloat(balance.innerText) + parseFloat(amount);
   } else {
@@ -18,15 +29,11 @@ btnAddMoney.addEventListener("click", function (event) {
 
 btnCashOut.addEventListener("click", function (event) {
   event.preventDefault();
-
   let balance = document.getElementById("balance");
   const amount = document.getElementById("amount").value;
   const pin = document.getElementById("pin").value;
-
-  console.log(amount, pin, myPin);
   if (myPin === pin) {
     balance.innerText = parseFloat(balance.innerText) - parseFloat(amount);
-    // console.log(balance, amount, pin, balance.innerText);
   } else {
     alert("Invalid Pin");
   }
